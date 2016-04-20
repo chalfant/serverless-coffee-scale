544a2c6bfd8dca0c6a13d72fb332d1e417c90469
Chris Chalfant <cchalfant@leafsoftwaresolutions.com>
2016-04-20 08:29:49 -0400

    firsties

---
+++ entries/createEntry/handler.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+import json
+import logging
+import boto3
+import os
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+table_name = os.getenv('DYNAMODB_TABLE_NAME')
+ddb = boto3.resource('dynamodb')
+table = ddb.Table(table_name)
+
+def handler(event, context):
+    log.debug("Received event {}".format(json.dumps(event)))
+
+    scale_id = event['scale_id']
+    timestamp = event['timestamp']
+    weight = event['weight']
+
+    item = {
+        'scale_id': scale_id,
+        'timestamp': timestamp,
+        'weight': weight
+    }
+
+    response = table.put_item(
+        Item=item
+    )
+
+    return item
